adead733260f4cebb73fafce0ce0f7aea12ab934
raulmtm64 <raulmtm64@gmail.com>
2025-06-18 14:06:57 +0200

    Login simple con DJANGO

--- back/apps/users/views.py
+++ back/apps/users/views.py
@@ -8,20 +8,30 @@ from .models import Users
 class Login(APIView):
     def post(self, request):
         data = request.data
-        user_identifier = list(data.keys())[0]
+
+        user_identifier = None
+        for key in ['nick', 'email']:
+            if key in data:
+                user_identifier = key
+                break
+
         user = data.get(user_identifier)
         password = request.data.get('password')
+        filter_kwargs = {user_identifier: user}
 
         if not user or not password:
-            return Response({"error":"Faltan datos"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error":"Missing data"}, status=status.HTTP_400_BAD_REQUEST)
         
         try:
-            user = Users.objects.get(user_identifier=user)
+            user = Users.objects.get(**filter_kwargs)
         except Users.DoesNotExist:
-            return Response({"error": "Usuario no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
         # if not check_password(password, user.password)    -->  PULIR
         if user.password != password:
-            return
+            return Response({"error": "Wrong password"}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        return Response({"msgLogin":"OK"})
+
         
             
